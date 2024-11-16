d7e5b4051fdb512b2ae92c683bb739c86e89ccc5
Jake <jake@proteatech.dev>
2024-11-15 18:07:38 -0800

    Add instance variable and a way to get the numerical value of a hand

--- hand.go
+++ hand.go
@@ -2,8 +2,17 @@ package main
 
 type Hand struct {
 	cards []Card
+	value int
 }
 
 func (h *Hand) addCard(c Card) {
 	h.cards = append(h.cards, c)
+}
+
+func (h *Hand) getHandValue() {
+	var total int
+	for _, c := range h.cards {
+		total += c.getValue()
+	}
+	h.value = total
 }
